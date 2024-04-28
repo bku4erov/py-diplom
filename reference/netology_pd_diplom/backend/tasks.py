f77edecd8a252cbcfb4a069462275543fac895ae
bku4erov <boris.ku4erov@gmail.com>
2024-04-28 12:05:47 +0300

    change import to async task

--- reference/netology_pd_diplom/backend/tasks.py
+++ reference/netology_pd_diplom/backend/tasks.py
@@ -1,7 +1,9 @@
 from celery import shared_task
 from celery.contrib.django.task import DjangoTask
 from django.core.mail import EmailMultiAlternatives
-from django.conf import settings
+from yaml import load as load_yaml, Loader
+
+from backend.models import Shop, Category, Product, ProductInfo, Parameter, ProductParameter
 
 @shared_task(base=DjangoTask)
 def send_email(title, message, sender, recipients):
@@ -17,3 +19,33 @@ def send_email(title, message, sender, recipients):
             recipients
         )
     msg.send()
+
+
+@shared_task
+def update_partner_info(stream, user_id):
+
+    data = load_yaml(stream, Loader=Loader)
+
+    shop, _ = Shop.objects.get_or_create(name=data['shop'], user_id=user_id)
+    for category in data['categories']:
+        category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
+        category_object.shops.add(shop.id)
+        category_object.save()
+    
+    ProductInfo.objects.filter(shop_id=shop.id).delete()
+
+    for item in data['goods']:
+        product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
+
+        product_info = ProductInfo.objects.create(product_id=product.id,
+                                                              external_id=item['id'],
+                                                              model=item['model'],
+                                                              price=item['price'],
+                                                              price_rrc=item['price_rrc'],
+                                                              quantity=item['quantity'],
+                                                              shop_id=shop.id)
+        for name, value in item['parameters'].items():
+            parameter_object, _ = Parameter.objects.get_or_create(name=name)
+            ProductParameter.objects.create(product_info_id=product_info.id,
+                                                        parameter_id=parameter_object.id,
+                                                        value=value)
